0883e974444b3647c8c9ce53def559671cd6aa84
haeter525 <cindejze412@gmail.com>
2023-11-02 20:45:49 +0800

    Update DP application

--- differential_privacy/privacy-preserving_research_data_sharing/general/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('check_file_status/', views.CheckFileStatus.as_view(), name = 'check_file_status'),
-]
--- differential_privacy/privacy-preserving_research_data_sharing/k_Anonymity/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class KAnonmityConfig(AppConfig):
-    name = 'k_Anonymity'
